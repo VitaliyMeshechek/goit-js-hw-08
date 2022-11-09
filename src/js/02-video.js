cc64191fd341d915bc71987ca050e35b56f2adf6
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-11-09 16:14:23 +0200

    task 1,2,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const refs = {
+  iframe: document.querySelector('#vimeo-player'),
+};
+    const player = new Vimeo.Player(refs.iframe);
+
+    player.on('timeupdate', throttle(data => {
+      localStorage.setItem('videoplayer-current-time', data.seconds)
+      }, 1000));
+
+
+  player.setCurrentTime(30.456).then(function(seconds) {
+    console.log(seconds);
+});
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormLocal, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+
+function onFormLocal(evt) {
+  object[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(object));
+     console.log(localStorage);
+};
+
+  const object = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    console.log(object);
+
+    onUpdatePage();
+
+function onFormSubmit(evt) {
+evt.preventDefault();
+evt.target.reset();
+localStorage.removeItem(STORAGE_KEY);
+};
+
+
+function onUpdatePage() {
+    Object.entries(object).forEach(([key, value]) => {
+        refs.form[key].value = value;
+    })
+};
